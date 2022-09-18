ab44319de45d6c1edbab655fc1caabf046c94811
collo9823 <collo9823@gmail.com>
2022-09-18 16:33:34 -0300

    commit

--- src/models.ts
+++ src/models.ts
@@ -9,11 +9,11 @@ class ContactsCollection {
   data: Contact[] = [];
   load() {
     // usar la version Async (readFile)
-    return jsonfile.readFile(__dirname + "/contacts.json").then((c) => {
+    const promesa = jsonfile.readFile(__dirname + "/contacts.json").then((c) => {
       this.data = c;
       console.log({ c });
     });
-    // this.data = json;
+    return promesa
   }
   getAll() {
     return this.data;
@@ -23,7 +23,8 @@ class ContactsCollection {
   }
   save() {
     // usar la version Async (writeFIle)
-    jsonfile.writeFileSync(__dirname + "/contacts.json", this.data);
+    // jsonfile.writeFileSync(__dirname + "/contacts.json", this.data);
+    return jsonfile.writeFile(__dirname+ '/contacts.json', this.data)
   }
   getOneById(id) {
     const encontrado = this.data.find((contacto) => {
@@ -36,3 +37,18 @@ class ContactsCollection {
   }
 }
 export { ContactsCollection, Contact };
+// function main(){
+//   const contactNew = new ContactsCollection;
+//   contactNew.load()
+//   const newContact = new Contact;
+//   newContact.id= 4;
+//   newContact.name= 'LEJOS LEJOS LEJOS'
+//   const newContact2 = new Contact;
+//   newContact2.id= 5;
+//   newContact2.name= 'lseonldibLEJOS LEJOS LEJOS'
+//   contactNew.addOne(newContact);
+//   contactNew.addOne(newContact2);
+//   contactNew.save()
+// }
+
+// main()
